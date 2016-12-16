0add4317fa2b4b17437494a2a784b351919fc807
mifl <mifl.pc@gmail.com>
2016-12-16 11:55:23 +0100

    ef65s: build: add support to build a full boot.img
    Change-Id: I5878aebee8462c840f89e32d61111af17ce963de

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,6 +4,7 @@
 # Kernel Build Script
 #
 ###############################################################################
+# 2016-12-15 mifl         : modified to generate a flashable boot.img
 # 2011-10-24 effectivesky : modified
 # 2010-12-29 allydrop     : created
 ###############################################################################
@@ -43,9 +44,41 @@ make ARCH=arm O=./$BUILD_KERNEL_OUT_DIR/$BUILD_KERNEL_OUT_SUBDIR/ ${DEFCONFIG_FI
 make -j$BUILD_JOB_NUMBER ARCH=arm O=./$BUILD_KERNEL_OUT_DIR/$BUILD_KERNEL_OUT_SUBDIR/ 2>&1 | tee $BUILD_KERNEL_LOG
 
 ###############################################################################
+# Set mkbootimg dir
+###############################################################################
+
+if [ "$DEFCONFIG_FILE" = "ef65s_defconfig" ]; then
+	MKBOOTIMGDIR=ef65s
+else
+	MKBOOTIMGDIR= ""
+fi
+
+###############################################################################
 # Copy Kernel Image
 ###############################################################################
 
 if [ -f ./$BUILD_KERNEL_OUT_DIR/$BUILD_KERNEL_OUT_SUBDIR/arch/arm/boot/zImage ]; then
-	cp -f ./$BUILD_KERNEL_OUT_DIR/$BUILD_KERNEL_OUT_SUBDIR/arch/arm/boot/zImage ./
+	if [ "$MKBOOTIMGDIR" != "" ]; then
+		cp -f ./$BUILD_KERNEL_OUT_DIR/$BUILD_KERNEL_OUT_SUBDIR/arch/arm/boot/zImage ./mkbootimg/${MKBOOTIMGDIR}
+	else
+		cp -f ./$BUILD_KERNEL_OUT_DIR/$BUILD_KERNEL_OUT_SUBDIR/arch/arm/boot/zImage ./
+	fi
+fi
+
+##############################################################################
+# Make BootImage
+##############################################################################
+
+# Prepare Device Tree Image (dt.img)
+if [ "$MKBOOTIMGDIR" != "" ]; then
+	pushd ./mkbootimg/${MKBOOTIMGDIR} > /dev/null
+	./makedtimg.sh
+	popd > /dev/null
+fi
+
+# Assembling the boot.img
+if [ "$MKBOOTIMGDIR" != "" ]; then
+	pushd ./mkbootimg/${MKBOOTIMGDIR} > /dev/null
+	./makebootimg.sh
+	popd > /dev/null
 fi
--- clean_kernel.sh
+++ clean_kernel.sh
@@ -4,6 +4,7 @@
 # Kernel Clean Script
 #
 ###############################################################################
+# 2016-12-15 mifl         : modified for clean mkbootimg dir
 # 2011-10-24 effectivesky : modified
 # 2010-12-29 allydrop     : created
 ###############################################################################
@@ -24,11 +25,23 @@ export PATH=$(pwd)/../../../prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin:$PATH
 export CROSS_COMPILE=arm-eabi-
 
 ###############################################################################
-# Clean: zImage, log file and obj dir
+# Clean: boot.img, dt.img, zImage, log file and obj dir
 ###############################################################################
 
 make mrproper
 make O=./$BUILD_KERNEL_OUT_DIR/$BUILD_KERNEL_OUT_SUBDIR/ clean
+
+if [ -f ./mkbootimg/*/boot.img ]; then
+	rm ./mkbootimg/*/boot.img
+fi
+
+if [ -f ./mkbootimg/*/zImage ]; then
+	rm ./mkbootimg/*/zImage
+fi
+
+if [ -f ./mkbootimg/*/dt.img ]; then
+	rm ./mkbootimg/*/dt.img
+fi
 
 if [ -f ./zImage ]; then
 	rm ./zImage
